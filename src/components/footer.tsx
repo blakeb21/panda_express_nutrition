3f8503ed72b3792199e8233aea0b274e4f1f7073
Blake Barnhill <blakeb21@vt.edu>
2023-08-11 13:49:00 -0400

    Link to blakebarnhill.com

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     const today = new Date();
   return (
 <footer className="flex bg-black w-full p-4 md:p-6 justify-center">
-	<span className="text-slate-400">&copy; {today.getFullYear()} Blake Barnhill. All rights reserved.</span>
+	<span className="text-slate-400">&copy; {today.getFullYear()} <a href="https://www.blakebarnhill.com">Blake Barnhill.</a> All rights reserved.</span>
 </footer>
 
   )
