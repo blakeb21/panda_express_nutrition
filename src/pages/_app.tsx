4cbd33e17e763f8a03300df20d2cf6e564c16db8
Blake Barnhill <blakeb21@vt.edu>
2024-10-04 15:01:32 -0400

    posthog rewrites

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,8 @@ import { PostHogProvider } from "posthog-js/react";
 if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   // checks that we are client-side
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     loaded: (posthog) => {
       if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
